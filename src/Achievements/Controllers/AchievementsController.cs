307f421e644ff2d37af82043e8fd3d39aa415809
siudzinski <tomek.siudzinski@gmail.com>
2023-04-06 19:07:07 +0200

    fix

--- src/Achievements/Controllers/AchievementsController.cs
+++ src/Achievements/Controllers/AchievementsController.cs
@@ -2,7 +2,6 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Achievements.Domain.CreatingAchievement;
 using Achievements.Domain.UnlockingAchievement;
-using System.Buffers;
 
 namespace Achievements.Controllers;
 
